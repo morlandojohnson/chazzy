74a780741bd32212f328f5cdcabf6e133e9076d8
Morlando Johnson <97807888+Lando241@users.noreply.github.com>
2025-06-06 13:40:30 -0400

    initialized bot

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const { Client, IntentsBitField } = require("discord.js");
+require("dotenv").config();
+
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
+});
+
+client.once("ready", () => {
+  console.log("Rise and grind babes");
+});
+
+client.on("ready", (c) => {
+  console.log(`${c.user.username} is alive`);
+});
+
+client.on("messageCreate", (message) => {
+  if (message.author.bot) {
+    return;
+  }
+  if (message.content === "hello") {
+    message.reply("hello");
+  }
+});
+
+client.login(process.env.TOKEN);
